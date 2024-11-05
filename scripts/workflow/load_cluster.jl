1dc83dc32e738a36fe67b2cbe9860cd00266db46
Alessio Quaresima <alessio.quaresima@pasteur.fr>
2024-11-05 13:06:49 +0100

    slurm cluster examples

--- scripts/workflow/load_cluster.jl
+++ scripts/workflow/load_cluster.jl
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name="test_run"            # Job Name
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=1                       # 1 CPU allocation per Task
+#SBATCH --mem=1GB
+#SBATCH --time=1:00:00
+#SBATCH --partition=common
+#SBATCH -q common
+#SBATCH -e slurm-test_julia_%j.err
+#SBATCH -o slurm-test_julia_%j.out
+#SBATCH --qos=fast
+
+#=
+srun julia $(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
+exit
+# =#
+
 using Distributed
 addprocs(4)
 @everywhere using DrWatson
