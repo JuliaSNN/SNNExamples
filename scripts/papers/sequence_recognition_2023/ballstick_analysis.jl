7cf8ee75651313d1b8148c3c5df0ef60d4d4d439
Alessio <alessio.quaresima@mpi.nl>
2024-12-03 15:41:18 +0100

    sequencerecognition

---
+++ scripts/papers/sequence_recognition_2023/ballstick_analysis.jl
@@ -0,0 +1,146 @@
+using DrWatson
+using Revise
+using SpikingNeuralNetworks
+SNN.@load_units;
+using SNNUtils
+using Plots
+using Statistics
+using Distributions
+
+# #
+model_config = (vd = -70mV, input_rate=8kHz)
+name = DrWatson.savename("recall_phase", model_config, "jld2")
+model_path = datadir("sequence_recognition", "overlap_lexicon", name) |> path -> (mkpath(dirname(path)); path) 
+@unpack model, seq, mytime, lexicon = load_model(model_path)
+##
+T = get_time(mytime)
+Trange = T-1s:1ms:T-100ms
+names, pops = filter_populations(model.stim) |> subpopulations
+pr1 = SNN.raster(model.pop.E, Trange, populations=pops, names=names)
+pr2 = SNN.raster(model.pop, Trange)
+plot(pr1, pr2, layout = (2, 1), size = (800, 800))
+# plot_activity(model, Trange)
+## Target activation with stimuli
+# Trange = 98000ms:1ms:100000ms
+
+# control = :POLLEN
+
+##
+using LaTeXStrings
+word = :GOD
+myintervals = sign_intervals(word, seq)
+membrane, r_v = SNN.interpolated_record(model.pop.E, :v_s)
+# membrane, r_v =  firing_rate(model.pop.E, τ=20ms)
+Trange = -500ms:1ms:diff(myintervals[1])[1]+500ms
+activity = zeros(length(seq.symbols.words),size(Trange,1))
+for w in eachindex(seq.symbols.words)
+    cells = getcells(stim, seq.symbols.words[w], :d)
+    n = 0
+    for myinterval in myintervals
+        _range = myinterval[1]-500ms:1ms:myinterval[2]+500ms
+        if _range[end] < r_v[end]
+            activity[w, :] += mean(membrane[cells, _range], dims=1)[1,:]
+            n+=1
+        end
+    end
+    activity[w, :] ./= n
+end
+plot(Trange, activity[:,:]', label=hcat(string.(seq.symbols.words)...), xlabel="Time (ms)", ylabel="Membrane potential (mV)", title="")
+vline!([0, diff(myintervals[1])[1]], c=:black, ls=:dash, label="")
+word_id = findfirst(seq.symbols.words .== word)
+plot!(Trange, activity[word_id,:], c=:black, label=string(word), lw=5, )
+
+##
+
+# @unpack stim = model
+# pv = plot()
+# cells = getcells(stim, word, :d)
+# control_cells = getcells(stim, control, :d)
+
+# SNN.vecplot!(pv, model.pop.E, :v_d, r = Trange, neurons = control_cells, dt = 0.125, pop_average = true, c = :grey, lw=2, ls=:dot)
+# SNN.vecplot!(pv, model.pop.E, :v_d, r = Trange, neurons = cells, dt = 0.125, pop_average = true, lw=3)
+# vline!(myintervals./1000, c = :black, ls=:dash)
+# plot!(title = "Depolarization of :AB vs :BA with stimuli AB")
+# plot!(xlabel = "Time (s)", ylabel = "Membrane potential (mV)")
+# plot!(margin = 5Plots.mm, ylims=:auto)
+# # plot(pv, pr, layout = (2, 1), size = (800, 800))
+# ##
+
+# ##
+# %%
+
+histogram(matrix(model.syn.E_to_E)[stim.POLL_d.cells, model.stim.L_d.cells][:], alpha=0.5, bins=1:0.5:45, label="L->POLL")
+histogram!(matrix(model.syn.E_to_E)[stim.POLL_d.cells, model.stim.P_d.cells][:], alpha=0.5, bins=1:0.5:45, label="P->POLL")
+plot!(title = "Synaptic weights from L and P to POLL")
+# average_weight(stim.A_d.cells, model.stim.AB_d.cells, model.syn.E_to_E)
+# average_weight(stim.AB_d.cells, model.stim.AB_d.cells, model.syn.E_to_E)
+
+##
+connections = zeros(length(pops), length(pops))
+indices = Int64[]
+names, pops = filter_populations(model.stim) |> subpopulations
+for pre in eachindex(pops)
+    for post in eachindex(pops)
+        pre_pop = pops[pre]
+        post_pop = pops[post]
+        # update_weight!(pre_pop, post_pop, model.syn.E_to_E)
+        connections[post, pre] = (average_weight(pre_pop, post_pop, model.syn.E_to_E) - mean(model.syn.E_to_E.W))./mean(model.syn.E_to_E.W)
+        append!(indices , weights_indices(pre_pop, post_pop, model.syn.E_to_E))
+    end
+end
+plasticity!(model.syn.norm, model.syn.norm.param)
+using LaTeXStrings
+heatmap(ticks=(1:length(names),names), connections, c=:amp, xlabel="Pre-synaptic population", ylabel="Post-synaptic population", title="Average synaptic weight "*L"(w_0)", size=(500,400), xrotation=45)
+vline!([10.5], c=:black, ls=:dash, label="")
+hline!([10.5], c=:black, ls=:dash, label="")
+
+
+## 
+## Recall phase
+names, pops = filter_populations(model.stim) |> subpopulations
+indices_words = Int64[]
+indices_phonemes = Int64[]
+indices_phtowords = Int64[]
+indices_wordtoph = Int64[]
+@unpack phonemes, words = symbolnames(seq)
+for pre in eachindex(pops)
+    for post in eachindex(pops)
+        pre_pop = pops[pre]
+        post_pop = pops[post]
+        if names[pre] ∈ phonemes && names[post] ∈ words
+            !(occursin(names[pre], names[post])) && continue
+            append!(indices_phtowords , weights_indices(pre_pop, post_pop, model.syn.E_to_E))
+        elseif names[pre] ∈ words && names[post] ∈ phonemes
+            !(occursin(names[post], names[pre])) && continue
+            append!(indices_wordtoph , weights_indices(pre_pop, post_pop, model.syn.E_to_E))
+        elseif names[pre] ∈ phonemes && names[post] ∈ phonemes
+            !(names[post] ==  names[pre]) && continue
+            append!(indices_phonemes , weights_indices(pre_pop, post_pop, model.syn.E_to_E))
+        elseif names[pre] ∈ words && names[post] ∈ words
+            !(names[post] ==  names[pre]) && continue
+            append!(indices_words , weights_indices(pre_pop, post_pop, model.syn.E_to_E))
+        else 
+            throw("No connection found")
+        end
+    end
+end
+
+
+##
+W, r_v = SpikingNeuralNetworks.interpolated_record(model.syn.E_to_E, :W)
+r = range(r_v[1], r_v[end], length = 1000)
+control_indices = setdiff(Set(axes(W, 1)), Set(vcat(indices_phonemes, indices_words, indices_phtowords, indices_wordtoph))) |> collect
+ave_weights = [mean(W[indices, r], dims=1)[1,:] for indices in [indices_phonemes, indices_words, indices_phtowords, indices_wordtoph, control_indices]]
+std_weights = [ std(W[indices, r], dims=1)[1,:] for indices in [indices_phonemes, indices_words, indices_phtowords, indices_wordtoph, control_indices]]
+
+
+labels = ["Phonemes to Phonemes" "Words to Words" "Phonemes to Words" "Words to Phonemes"]
+plot(
+    plot(mean(W, dims=1)[1,:], label="Average weight", xlabel="Time (ms)", ylabel="Weight"),
+    plot(r,ave_weights, ribbon = std_weights'./5, label="", xlabel="Time (ms)", ylabel="Weight", labels= labels),
+    layout = (2, 1),
+    size = (500, 800),
+    margin = 5Plots.mm
+)
+##
+
--- scripts/papers/sequence_recognition_2023/ballstick_quaresima2023.jl
+++ scripts/papers/sequence_recognition_2023/ballstick_quaresima2023.jl
@@ -8,16 +8,22 @@ using Statistics
 using Distributions
 
 ##
-function run_network(params, name)
-    @unpack exc, pv, sst, plasticity, connectivity = params
+
+include("../../parameters/dendritic_network.jl")
+function get_network(params, name)
+    # @unpack exc, pv, sst, plasticity, connectivity = params
+    pv = duarte2019.PV
+    sst = duarte2019.SST
+    @unpack connectivity, plasticity = quaresima2023
+    @unpack dends, NMDA, param, soma_syn, dend_syn = quaresima2022
     # Number of neurons in the network
-    NE = 1000
+    NE = 1200
     NI = NE ÷ 4
     NI1 = round(Int,NI * 0.35)
     NI2 = round(Int,NI * 0.65)
     # Import models parameters
     # Define interneurons I1 and I2
-    @unpack dends, NMDA, param, soma_syn, dend_syn = exc
+    # @unpack dends, NMDA, param, soma_syn, dend_syn = exc
     E = SNN.BallAndStickHet(; N = NE, soma_syn = soma_syn, dend_syn = dend_syn, NMDA = NMDA, param = param, name="Exc")
     I1 = SNN.IF(; N = NI1, param = pv, name="I1_pv")
     I2 = SNN.IF(; N = NI2, param = sst, name="I2_sst")
@@ -28,13 +34,14 @@ function run_network(params, name)
     I1_to_E = SNN.SpikingSynapse(I1, E, :hi, :s; param = plasticity.iSTDP_rate, connectivity.EIf...)
     I1_to_I1 = SNN.SpikingSynapse(I1, I1, :gi; connectivity.IfIf...)
     I1_to_I2 = SNN.SpikingSynapse(I1, I2, :gi; connectivity.IfIs...)
-    I2_to_I2 = SNN.SpikingSynapse(I1, I2, :gi; connectivity.IsIs...)
+    I2_to_I2 = SNN.SpikingSynapse(I2, I2, :gi; connectivity.IsIs...)
     I2_to_E = SNN.SpikingSynapse(I2, E, :hi, :d; param = plasticity.iSTDP_potential, connectivity.EdIs...)
     I2_to_I1 = SNN.SpikingSynapse(I2, I1, :gi; connectivity.IsIf...)
     # Define normalization
     norm = SNN.SynapseNormalization(NE, [E_to_E], param = SNN.MultiplicativeNorm(τ = 20ms))
     # background noise
     stimuli = Dict(
+        :s   => SNN.PoissonStimulus(E,  :he_s,  param=4.0kHz, cells=:ALL, μ=1.f0, name="noise_d",),
         :d   => SNN.PoissonStimulus(E,  :he_d,  param=2.0kHz, cells=:ALL, μ=1.f0, name="noise_d",),
         :i1  => SNN.PoissonStimulus(I1, :ge,   param=1.5kHz, cells=:ALL, μ=1.f0,  name="noise_i1"),
         :i2  => SNN.PoissonStimulus(I2, :ge,   param=2.0kHz, cells=:ALL, μ=1.8f0, name="noise_i2")
@@ -48,53 +55,60 @@ end
 
 
 # Define the network
-network = run_network(bursty_dendritic_network, "bursty_dendritic_network" )
+network = get_network(bursty_dendritic_network, "bursty_dendritic_network" )
+dictionary = getdictionary(["POLLEN", "GOLD", "GOLDEN", "DOLL", "LOP", "GOD", "LOG", "POLL", "GOAL", "DOG"])
+duration = getduration(dictionary, 50ms)
 
 
-# Stimulus
-dictionary = Dict(:AB=>[:A, :B], :BA=>[:B, :A])
-duration = Dict(:A=>50, :B=>50, :_=>50)
 config_lexicon = ( ph_duration=duration, dictionary=dictionary)
-config_sequence = (init_silence=1s, seq_length=100, silence=1,)
+config_sequence = (init_silence=1s, repetition=150, silence=1,)
 lexicon = generate_lexicon(config_lexicon)
-
 stim, seq = SNNUtils.step_input_sequence(network = network, 
+                                    words=true,
                                     targets= [:d],
                                     lexicon = lexicon, 
-                                    config_sequence = config_sequence, 
-                                    peak_rate=4kHz, 
-                                    start_rate=4kHz, 
+                                    peak_rate=8kHz, 
+                                    start_rate=8kHz, 
                                     decay_rate=10ms,
-                                    p_post = 0.15f0)
+                                    p_post = 0.05f0;
+                                    config_sequence..., 
+                                    )
+
+
+
+##
+name = DrWatson.savename("associative_phase", model_config, "jld2")
+model_path = datadir("sequence_recognition", "overlap_lexicon", name) |> path -> (mkpath(dirname(path)); path)
 
 # Merge network and stimuli in model
 model = merge_models(network, stim)
-
-# SNN.monitor(network.pop.E, [:fire, :v_d, :v_s])p
+model_config = (vd = -70mV, input_rate=8kHz)
 SNN.monitor([model.pop...], [:fire])
 SNN.monitor([model.pop...], [ :v_d, :v_s], sr=200Hz)
+SNN.monitor([model.syn...], [ :W], sr=10Hz)
 duration = sequence_end(seq)
 mytime = SNN.Time()
 SNN.train!(model=model, duration= duration, pbar=true, dt=0.125, time=mytime)
-##
-T = get_time(mytime)
-Trange = T-2s:1ms:T
-names, pops = filter_populations(model.stim, "noise") |> subpopulations
-pr = SNN.raster(network.pop.E, Trange, populations=pops, names=names)
 
-## Target activation with stimuli
-@unpack stim = model
-pv = plot()
-cells = collect(intersect(Set(stim.AB_d.cells)))
-control_cells = rand(1:model.pop.E.N, length(cells))
-control_cells = collect(intersect(Set(stim.BA_d.cells)))
+DrWatson.save(model_path, @strdict model seq mytime lexicon config_sequence config_lexicon)
+filesize(model_path) |> Base.format_bytes
+basename(model_path)
 
-SNN.vecplot!(pv, model.pop.E, :v_d, r = Trange, neurons = control_cells, dt = 0.125, pop_average = true, c = :grey, lw=2, ls=:dot)
-SNN.vecplot!(pv, model.pop.E, :v_d, r = Trange, neurons = cells, dt = 0.125, pop_average = true, lw=3)
-myintervals = sign_intervals(:AB, seq)
-vline!(myintervals./1000, c = :black, ls=:dash)
-plot!(title = "Depolarization of :AB vs :BA with stimuli AB")
-plot!(xlabel = "Time (s)", ylabel = "Membrane potential (mV)")
-plot!(margin = 5Plots.mm)
-plot(pv, pr, layout = (2, 1), size = (800, 800))
-# %%
+## Recall phase
+name = DrWatson.savename("recall_phase", model_config, "jld2")
+model_path = datadir("sequence_recognition", "overlap_lexicon", name) |> path -> (mkpath(dirname(path)); path)
+
+@unpack model, seq, mytime, lexicon = copy_model(model_path)
+seq = randomize_sequence!(;seq=seq, model=model, target=:d, words=false, config_sequence...)
+config_sequence = (init_silence=1s, repetition=150, silence=1,)
+model.syn.E_to_E.param.active[1]=false
+SNN.monitor([model.pop...], [:fire])
+SNN.monitor([model.pop...], [ :v_d, :v_s], sr=200Hz)
+SNN.monitor([model.syn...], [ :W], sr=10Hz)
+duration = sequence_end(seq)
+mytime = SNN.Time()
+SNN.train!(model=model, duration= duration, pbar=true, dt=0.125, time=mytime)
+
+DrWatson.save(model_path, @strdict model seq mytime lexicon config_sequence config_lexicon)
+filesize(model_path) |> Base.format_bytes
+basename(model_path)
