968ef0549e4deb1b53119316fb9bc8ecc2f9b81b
Alessio <alessio.quaresima@mpi.nl>
2024-12-05 16:04:13 +0100

    training to SNNUtils

--- scripts/papers/sequence_recognition_2023/ballstick_training.jl
+++ scripts/papers/sequence_recognition_2023/ballstick_training.jl
@@ -1,8 +1,4 @@
 ##
-using MLJ
-using LIBSVM
-using StatsBase
-using Statistics
 
 
 model_info = (repetition=200, 
@@ -14,56 +10,36 @@ path = datadir("sequence_recognition", "overlap_lexicon")
 data = load_data(path, "recall_phase", model_info)
 @unpack model, seq, mytime, lexicon = data
 
-##
-function get_intervals(model, interval=[-50ms, 100ms])
-    N = model.pop.E.N
-    offsets = Vector{Vector{Float32}}()
-    ys = Vector{Symbol}()
-    for word in seq.symbols.words
-        for myinterval in sign_intervals(word, seq)
-            offset = myinterval[end] .+ interval
-            push!(offsets, offset)
-            push!(ys, word)
-        end
-    end
-    return offsets, ys, N
-end
+offsets, ys = all_intervals(:words, seq, interval=[-50ms, 100ms])
+@unpack N = model.pop.E
 
-function train(Xs, ys, name)
-    Xs = Xs .+ 1e-1
-    Xtrain = Xs[:, 1:2:end]
-    Xtest  = Xs[:, 2:2:end]
-    ytrain = string.(ys[1:2:end])
-    ytest  = string.(ys[2:2:end])
-    ZScore = fit(StatsBase.ZScoreTransform,Xtrain, dims=2)
-    Xtrain = StatsBase.transform(ZScore, Xtrain)
-    Xtest = StatsBase.transform(ZScore, Xtest)
 
-    classifier = svmtrain(Xtrain, ytrain)
-    # Test model on the other half of the data.
-    ŷ, decision_values = svmpredict(classifier, Xtest);
-    @printf "%s Accuracy: %.2f%%\n" name mean(ŷ .== ytest) * 100
-end
-
-offsets, ys, N = get_intervals(model)
-
-FR = zeros(N, length(ys))
-Threads.@threads for i in eachindex(offsets)
-    offset = offsets[i]
-    FR[:,i] = length.(spiketimes(model.pop.E, interval = offset))
-end
 
 M = zeros(N, length(ys))
-membrane, r_v = SNN.interpolated_record(model.pop.E, :v_s)
 Threads.@threads for i in eachindex(offsets)
     offset = offsets[i]
-    offset[end] > r_v[end] && continue
     range = offset[1]:1ms:offset[2]
     M[:,i] = mean(membrane[:, range], dims=2)[:,1]
 end
 
-train(M, ys, "Membrane potential")
-train(Xs, ys, "Firing rate")
+M
+
+##
+μ = mean(scores)
+xs = exp.(-4:0.2:-1.5)
+scores = map(xs) do x
+    train(M, ys; name= "Membrane potential", seed=123, p=x)
+end
+k = (scores .- μ) ./ (1-μ)
+
+plot(xs, k, xlabel="Train/Test size", ylabel="Accuracy", legend=false)
+
+##
+
+[train(M, ys; name= "Membrane potential", seed=x) for x in 1:20]
+
+
+predict(mach, Xnew)train(Xs, ys, "Firing rate")
 
 
 average_activity = zeros(length(seq.symbols.words), N)
