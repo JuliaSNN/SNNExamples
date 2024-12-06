fbebbbc1dea42e2512fd09ba6953829bc01b7463
Russell Jarvis <russelljarvis42@gmail.com>
2024-12-06 14:21:25 +1100

    incremental improvement of the accuracy of the PD model

--- scripts/papers/multilayer_potjans-diesmann2015.jl
+++ scripts/papers/multilayer_potjans-diesmann2015.jl
@@ -10,7 +10,8 @@ using ProgressMeter
 using Plots
 using SpikingNeuralNetworks
 using SNNUtils
-
+using JLD2
+using Distributions
 
 """
 Auxiliary Potjans parameters for neural populations with scaled cell counts
@@ -30,7 +31,8 @@ function potjans_neurons(scale=1.0)
     neurons = Dict{Symbol, SNN.AbstractPopulation}()
     for (k, v) in ccu
         if occursin("E", String(k))
-            neurons[k] = AdEx(N = v, param=LKD2014SingleExp.AdEx, name=string(k))
+	    # PD model only consists of IF neurons.
+            neurons[k] = IF(N = v, param=LKD2014SingleExp.PV, name=string(k))
         else
             neurons[k] = IF(N = v, param=LKD2014SingleExp.PV, name=string(k))
         end
@@ -42,22 +44,34 @@ end
 Define Potjans parameters for neuron populations and connection probabilities
 """
 function potjans_conn(Ne)
-
-    function j_from_name(pre, post, g)
+    # adding static synaptic weight parameters 
+    # based on PSPs that are more accurate to PD based on the PyNN
+    # model
+    function j_from_name(pre, post)
         if occursin("E", String(pre)) && occursin("E", String(post))
-            return g.jee
+            syn_weight_dist = Normal(0.15, 0.1)
+            
+            return rand(syn_weight_dist)
         elseif occursin("I", String(pre)) && occursin("E", String(post))
-            return g.jei
+            syn_weight_dist = Normal(0.15, 0.1)
+            return -4.0*rand(syn_weight_dist)
+
         elseif occursin("E", String(pre)) && occursin("I", String(post))
-            return g.jie
+            syn_weight_dist = Normal(0.15, 0.1)
+            
+            return rand(syn_weight_dist)
         elseif occursin("I", String(pre)) && occursin("I", String(post))
-            return g.jii
+            syn_weight_dist = Normal(0.15, 0.1)
+            return -4.0*rand(syn_weight_dist)
         else 
             throw(ArgumentError("Invalid pre-post combination: $pre-$post"))
         end
     end
 
+    
+
     layer_names = [:E23, :I23, :E4, :I4, :E5, :I5, :E6, :I6]
+
 
     # Replace static matrix with a regular matrix for `conn_probs`
     # ! the convention is j_post_pre. This is how the matrices `w` are built. Are you using that when defining the parameters?
@@ -72,31 +86,62 @@ function potjans_conn(Ne)
         0.0364  0.001  0.0034 0.0005 0.0277 0.008   0.0658 0.1443
     ]
 
-    # !Assign dimensions to these parameters, and some reference
-    pree = 0.1
-    g = 1.0
-    tau_meme = 10.0   # (ms)
-    # Synaptic strengths for each connection type
-    K = round(Int, Ne * pree)
-    sqrtK = sqrt(K)
 
-    # !Same, the convention is j_post_pre
-    je = 2.0 / sqrtK * tau_meme * g
-    ji = 2.0 / sqrtK * tau_meme * g 
-    jee = 0.15 * je
-    jie = je
-    jei = -0.75 * ji
-    jii = -ji
-    g_strengths = dict2ntuple(SNN.@symdict jee jie jei jii)
-    
     conn_j = zeros(Float32, size(conn_probs))
     for pre in eachindex(layer_names)
         for post in eachindex(layer_names)
-            conn_j[post, pre ] = j_from_name(layer_names[pre], layer_names[post], g_strengths)
+            
+            conn_j[post, pre ] = j_from_name(layer_names[pre], layer_names[post])     
+
         end
     end
 
     return layer_names, conn_probs, conn_j
+end
+
+function unused_volumetric_part_of_model()
+
+    total_cortical_thickness = 1500.0
+    N_full = [20683, 5834, 21915, 5479, 4850, 1065, 14395, 2948] 
+    N_E_total = N_full[1]+N_full[3]+N_full[5]+N_full[7]
+    dimensions_3D = Dict(
+        "x_dimension"=> 1000,
+        "z_dimension"=>  1000,
+        "total_cortical_thickness"=> total_cortical_thickness,
+
+        # Have the thicknesses proportional to the numbers of E cells in each layer
+        "layer_thicknesses"=> Dict(
+        "L23"=> total_cortical_thickness*N_full[1]/N_E_total,
+        "L4" => total_cortical_thickness*N_full[3]/N_E_total,
+        "L5" => total_cortical_thickness*N_full[5]/N_E_total,
+        "L6" => total_cortical_thickness*N_full[7]/N_E_total,
+        "thalamus" => 100
+        )
+    )
+    
+    net_dict = Dict{String, Any}(
+        "PSP_e"=> 0.15,
+        # Relative standard deviation of the postsynaptic potential.
+        "PSP_sd"=> 0.1,
+        # Relative inhibitory synaptic strength (in relative units).
+        "g"=> -4,
+        # Rate of the Poissonian spike generator (in Hz).
+        "bg_rate"=> 8.,
+        # Turn Poisson input on or off (True or False).
+        "poisson_input"=> true,
+        # Delay of the Poisson generator (in ms).
+        "poisson_delay"=> 1.5,
+        # Mean delay of excitatory connections (in ms).
+        "mean_delay_exc"=> 1.5,
+        # Mean delay of inhibitory connections (in ms).
+        "mean_delay_inh"=> 0.75,
+        # Relative standard deviation of the delay of excitatory and
+        # inhibitory connections (in relative units).
+        "rel_std_delay"=> 0.5
+    )
+    dimensions_3D,net_dict
+
+
 end
 
 
@@ -111,8 +156,13 @@ function potjans_layer(scale)
     inh_pop = filter(x -> occursin("I", String(x)), keys(neurons))
     Ne = trunc(Int32, sum([neurons[k].N for k in exc_pop]))
     Ni = trunc(Int32, sum([neurons[k].N for k in inh_pop]))
-    @show exc_pop, Ne, Ni
     layer_names, conn_probs, conn_j = potjans_conn(Ne)
+    ##
+    # Added synaptic delays from distribution informed by PyNN/PD model
+    ##
+    syn_weight_dist = Normal(0.15, 0.1)
+    delay_dist_exc = Normal(1.5, 0.5)
+    delay_dist_inh = Normal( 0.75, 0.5)
 
     ## Create the synaptic connections based on the connection probabilities and synaptic weights assigned to each pre-post pair
     connections = Dict()
@@ -124,24 +174,30 @@ function potjans_layer(scale)
             J = conn_j[j, i]
             sym = J>=0 ? :ge : :gi
             μ = abs(J)
-            s = SNN.SpikingSynapse(neurons[pre], neurons[post], sym; μ = μ, p=p, σ=0)
+            if J>=0        
+                s = SNN.SpikingSynapse(neurons[pre], neurons[post], sym; μ = μ, p=p, σ=0, delay_dist=delay_dist_exc)
+            else
+                s = SNN.SpikingSynapse(neurons[pre], neurons[post], sym; μ = -μ, p=p, σ=0, delay_dist=delay_dist_inh)
+            
+            end
             connections[Symbol(string(pre,"_", post))] = s
         end
     end
 
     ## Create the Poisson stimulus for each population
+    full_mean_rates = [0.971, 2.868, 4.746, 5.396, 8.142, 9.078, 0.991, 7.523]
     stimuli = Dict()
-    for pop in exc_pop
-        νe = 2.5kHz
+    for (ind,pop) in enumerate(exc_pop)
+        νe = full_mean_rates[ind]kHz
         post = neurons[pop]
         s = SNN.PoissonStimulus(post, :ge; param = νe, cells=:ALL, μ=1.f0, name="PoissonE_$(post.name)")
         stimuli[Symbol(string("PoissonE_", pop))] = s
     end
-    return merge_models(neurons, connections, stimuli)
+    return merge_models(neurons,connections, stimuli),neurons,connections,stimuli
 end
 
 
-model = potjans_layer(0.01)
+model,neurons,connections,stimuli = potjans_layer(0.01)
 duration = 15000ms
 SNN.monitor([model.pop...], [:fire])
 SNN.monitor([model.pop...], [:v], sr=200Hz)
