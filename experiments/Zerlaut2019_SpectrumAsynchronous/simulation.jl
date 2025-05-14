7c63e39a63d10d176f3e3a929852a0936805c3ba
Alessio Quaresima <aquaresi@pasteur.fr>
2025-05-14 18:21:54 +0200

    simulation example for Zerlaut network

--- experiments/Zerlaut2019_SpectrumAsynchronous/simulation.jl
+++ experiments/Zerlaut2019_SpectrumAsynchronous/simulation.jl
@@ -1,10 +1,11 @@
 using DrWatson
 using Distributions
 using YAML
+using ThreadTools
 
 using SpikingNeuralNetworks
-using SNNPlots
 using SNNUtils
+using SNNPlots
 SNN.@load_units
 include(projectdir("loaders", "filesystem.jl"))
 begin
@@ -15,45 +16,60 @@ end
 #
 
 
-exp.(log.(1:20))
-f_rate = map(0:2.5:50) do x
-    config = @update Zerlaut2019_network begin
-        afferents.rate = x*Hz
-    end 
-    model = soma_network(config)
-    sim!(;model, duration=10_000ms,  pbar=true)
-    fr, r= firing_rate(model.pop.E, interval=3s:10s, pop_average=true)
-    f = mean(fr)
+νa =  exp.(range(log(1), log(40), 20))
+f_rate = map(νa) do x
+    frs = tmap(1:5) do _
+        config = @update Zerlaut2019_network begin
+            afferents.rate = x*Hz
+        end 
+        model = soma_network(config)
+        sim!(;model, duration=10_000ms,  pbar=false)
+        fr= firing_rate(model.pop.E, interval=3s:10s, pop_average=true, time_average=true)
+    end
+        f =     mean(frs)
     @info "rate: $x Hz = $f"
-    f
+    frs
 end
-
-plot(f_rate)#, xscale=:log, yscale=:log)
 ##
-# raster(model.pop, 0s:1s, every=10)
-# vecplot(model.pop.E, :v, interval=0s:0.5s, neurons=1:1)
+
+ff_rate = [filter(x -> x < 80, mean.(fr)) for fr in f_rate]
+SNNPlots.scatter(νa, mean.(ff_rate), ribbon=std.(ff_rate), scale=:log10, xlims=(1,20), ylims=(0.00001,80))#, xscale=:log, yscale=:log)
+SNNPlots.plot!(νa, mean.(ff_rate), ribbon=std.(ff_rate), scale=:log10, xlims=(1, 20), ylims=(0.00001,80), lw=5, xticks=([1,5,10, 20], [1,5,10,20]))#, xscale=:log, yscale=:log)
+plot!(xlabel="Afferent rate (Hz)", ylabel="Firing rate (Hz)",  legend=false, size=(400,400))
+
+
+
+
+
+
+
+
+
+# ##
+# # raster(model.pop, 0s:1s, every=10)
+# # vecplot(model.pop.E, :v, interval=0s:0.5s, neurons=1:1)
+# # plot(fr)
+
+
+# # raster(model.stim, 0s:1s)
+# ##
+
+
+# model = soma_network(Zerlaut2019_network)
+# sim!(;model, duration=3000ms,  pbar=true)
+# fr, r= firing_rate(model.pop.E, interval=2s:3s, pop_average=true)
+
 # plot(fr)
+# ##
+# raster(model.pop, 0s:3s, every=1)
 
+# fr, r= firing_rate(model.pop.E, interval=2s:3s, pop_average=true)
+# vecplot(model.pop.E, :v, interval=0s:1s, neurons=1:1)
+# plot(fr)
+# ##
 
-# raster(model.stim, 0s:1s)
-##
+# length(model.stim.afferentE.W) /100
 
-
-model = soma_network(Zerlaut2019_network)
-sim!(;model, duration=3000ms,  pbar=true)
-fr, r= firing_rate(model.pop.E, interval=2s:3s, pop_average=true)
-
-plot(fr)
-##
-raster(model.pop, 0s:3s, every=1)
-
-fr, r= firing_rate(model.pop.E, interval=2s:3s, pop_average=true)
-vecplot(model.pop.E, :v, interval=0s:1s, neurons=1:1)
-plot(fr)
-##
-
-length(model.stim.afferentE.W) /100
-
-model.stim.afferentE.I
+# model.stim.afferentE.I
 
 
--- experiments/Zerlaut2019_SpectrumAsynchronous/simulation_example.jl
+++ experiments/Zerlaut2019_SpectrumAsynchronous/simulation_example.jl
@@ -1,4 +1,19 @@
-# 
+using Pkg
+Pkg.add("ThreadTools")
+Pkg.add("Plots")
+Pkg.add("UnPack")
+Pkg.add("Statistics")
+Pkg.add("Plots")
+Pkg.add(url="https://github.com/JuliaSNN/SpikingNeuralNetworks.jl")
+
+##
+using ThreadTools
+using Plots
+using UnPack
+using Statistics
+using SpikingNeuralNetworks
+SNN.@load_units
+
 Zerlaut2019_network = (
     Npop = (E=4000, I=1000),
 
@@ -37,7 +52,7 @@ Zerlaut2019_network = (
     
     afferents = (
         N = 100,
-        ϵ = 0.1f0,
+        p = 0.1f0,
         rate = 20Hz,
         μ = 4.0,
         ), 
@@ -48,9 +63,9 @@ function soma_network(config)
     E = IF(N=Npop.E, param=config.exc, name="E")
     I = IF(N=Npop.I, param=config.inh, name="I")
 
-    AfferentParam = PoissonStimulusLayer(afferents.N; rate=afferents.rate, ϵ=afferents.ϵ)
-    afferentE = PoissonLayer(E, :ge, μ=afferents.μ, param=AfferentParam, name="noiseE")
-    afferentI = PoissonLayer(I, :ge, μ=afferents.μ, param=AfferentParam, name="noiseI")
+    AfferentParam = PoissonStimulusLayer(afferents.rate; afferents...)
+    afferentE = PoissonLayer(E, :ge, param=AfferentParam, name="noiseE")
+    afferentI = PoissonLayer(I, :ge, param=AfferentParam, name="noiseI")
 
     synapses = (
         E_to_E = SpikingSynapse(E, E, :ge, p=connections.E_to_E.p, μ=connections.E_to_E.μ, name="E_to_E"),
@@ -64,3 +79,28 @@ function soma_network(config)
     # monitor!(model.pop, [:v], sr=200Hz)
     return merge_models(;model..., silent=true)
 end
+
+
+
+νa =  exp.(range(log(1), log(20), 20))
+f_rate = map(νa) do x
+    frs = tmap(1:5) do _
+        config = @update Zerlaut2019_network begin
+            afferents.rate = x*Hz
+        end 
+        model = soma_network(config)
+        sim!(;model, duration=10_000ms,  pbar=false)
+        fr= firing_rate(model.pop.E, interval=3s:10s, pop_average=true, time_average=true)
+    end
+        f =     mean(frs)
+    @info "rate: $x Hz = $(mean(f))"
+    frs
+end
+
+ff_rate = [filter(x -> x < 80, mean.(fr)) for fr in f_rate]
+scatter(νa, mean.(ff_rate), ribbon=std.(ff_rate), scale=:log10, xlims=(0.9,20), ylims=(0.0000001,80))#, xscale=:log, yscale=:log)
+plot!(νa, mean.(ff_rate), ribbon=std.(ff_rate), scale=:log10, xlims=(0.9, 20), ylims=(0.0000001,80), lw=5, xticks=([1,5,10, 20], [1,5,10,20]))#, xscale=:log, yscale=:log)
+plot!(xlabel="Afferent rate (Hz)", ylabel="Firing rate (Hz)",  legend=false, size=(400,400))
+
+
+
